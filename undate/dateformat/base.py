2fc03d71d19fc5af0baa6b2ec39166722492dce6
Cole Crawford <cole_crawford@fas.harvard.edu>
2022-11-09 12:01:44 -0500

    Basic type hints for Undate and formatters
    Some suppressed errors. Using typing.Union[] style typing for compatibility with Python 3.8 and below

--- undate/dateformat/base.py
+++ undate/dateformat/base.py
@@ -1,20 +1,25 @@
 # base class for date format parsers
+from typing import Union, Dict
+from undate.undate import Undate, UndateInterval
 
 
 class BaseDateFormat:
     """Base class for parsing and formatting dates for specific formats."""
 
     # Subclasses should define a unique name.
-    name = "Base Formatter"
+    name: str = "Base Formatter"
 
-    def parse(self, value):
+    def parse(self, value) -> Union[Undate, UndateInterval]:
         # should return an undate or undate interval
         raise NotImplementedError
 
-    def to_string(self, undate):
+    def to_string(self, undate: Union[Undate, UndateInterval]) -> str:
         # convert an undate or interval to string representation for this format
         raise NotImplementedError
 
     @classmethod
-    def available_formatters(cls):
-        return {c.name: c for c in cls.__subclasses__()}
+    def available_formatters(cls) -> Dict[str, "BaseDateFormat"]:
+        # TODO: fix type error
+        # Expression of type "dict[str, Type[BaseDateFormat]]" cannot be assigned to return type "Dict[str, BaseDateFormat]"
+        # "dict[str, Type[BaseDateFormat]]" is incompatible with "Dict[str, BaseDateFormat]"
+        return {c.name: c for c in cls.__subclasses__()}  # type: ignore
--- undate/undate.py
+++ undate/undate.py
@@ -1,6 +1,9 @@
 import datetime
 from calendar import monthrange
 
+# Pre 3.10 requires Union for multiple types, e.g. Union[int, None] instead of int | None
+from typing import Optional, Dict, Union
+
 from undate.dateformat.base import BaseDateFormat
 
 
@@ -9,12 +12,18 @@ class Undate:
 
     DEFAULT_FORMAT = "ISO8601"
 
-    earliest = None
-    latest = None
-    label = None
-    formatter = None
+    earliest: Union[datetime.date, None] = None
+    latest: Union[datetime.date, None] = None
+    label: Union[str, None] = None
+    formatter: Union[BaseDateFormat, None] = None
 
-    def __init__(self, year=None, month=None, day=None, formatter=None):
+    def __init__(
+        self,
+        year: Optional[int] = None,
+        month: Optional[int] = None,
+        day: Optional[int] = None,
+        formatter: Optional[BaseDateFormat] = None,
+    ):
         # TODO: support initializing for unknown values in each of these
         # e.g., maybe values could be string or int; if string with
         # unknown digits, calculate min/max for unknowns
@@ -25,29 +34,36 @@ class Undate:
         # if day is unknown but we have year and month, calculate max day
         if day is None and year and month:
             _, maxday = monthrange(year, month)
+        elif day is None and year is None and month:
+            # TODO: what to do if we don't have year and month?
+            # This will produce bad data if the year is a leap year and the month is February
+            # 2022 chosen below as it is not a not leap year
+            # Better than just setting 31, but still not great
+            _, maxday = monthrange(2022, month)
         else:
-            maxday = 31  # ???
+            maxday: int = 31
         self.latest = datetime.date(
             year or datetime.MAXYEAR, month or 12, day or maxday
         )
         # keep track of which values are known
-        self.known_values = {
+        self.known_values: Dict[str, int] = {
             "year": year is not None,
             "month": month is not None,
             "day": day is not None,
         }
 
         if not formatter:
+            # TODO subclass definitions not available unless they are imported where Undate() is called
             formatter = BaseDateFormat.available_formatters()[self.DEFAULT_FORMAT]()
         self.formatter = formatter
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.formatter.to_string(self)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return "<Undate %s>" % self
 
-    def __eq__(self, other):
+    def __eq__(self, other: "Undate") -> bool:
         # question: should label be taken into account when checking equality?
         # for now, assuming label differences don't matter for comparing dates
         return (
@@ -60,15 +76,17 @@ class Undate:
 class UndateInterval:
     # date range between two uncertain dates
 
-    def __init__(self, earliest=None, latest=None):
+    def __init__(
+        self, earliest: Union[Undate, None] = None, latest: Union[Undate, None] = None
+    ):
         # for now, assume takes two undate objects
         self.earliest = earliest
         self.latest = latest
 
-    def __str__(self):
+    def __str__(self) -> str:
         # using EDTF syntax for open ranges
         return "%s/%s" % (self.earliest or "..", self.latest or "")
 
-    def __eq__(self, other):
+    def __eq__(self, other) -> bool:
         # consider interval equal if both dates are equal
         return self.earliest == other.earliest and self.latest == other.latest
