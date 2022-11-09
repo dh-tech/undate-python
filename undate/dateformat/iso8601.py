c7026783e4602387cbc77537270cfaf90a9c19cc
Cole Crawford <16374762+ColeDCrawford@users.noreply.github.com>
2022-11-09 16:19:49 -0500

    Feature/11 type hints (#16)
    * Preliminary structure for format-specific parsing #2
    
    Plus equality methods for undate objects
    
    Co-authored-by: Julia Damerow <jdamerow@users.noreply.github.com>
    
    * Added test cases for UndateInterval eq and not equal
    
    Co-authored-by: Rebecca Sutton Koeser <rlskoeser@users.noreply.github.com>
    
    * Added test cases for eq and unequal for Undate and UndateInterval.
    
    Co-authored-by: Rebecca Sutton Koeser <rlskoeser@users.noreply.github.com>
    
    * Added formatter logic
    
    added available formatter method to formatter base class and added default formatter to undate class. Undate class is no using the default formatter for the string method.
    
    Co-authored-by: Rebecca Sutton Koeser <rlskoeser@users.noreply.github.com>
    
    * Cleaned up tests
    
    Co-authored-by: Rebecca Sutton Koeser <rlskoeser@users.noreply.github.com>
    
    * Making black happy
    
    Co-authored-by: Rebecca Sutton Koeser <rlskoeser@users.noreply.github.com>
    
    * making tests happy
    
    Co-authored-by: Rebecca Sutton Koeser <rlskoeser@users.noreply.github.com>
    
    * Basic type hints for Undate and formatters
    
    Some suppressed errors. Using typing.Union[] style typing for compatibility with Python 3.8 and below
    
    * Fix failing tests due to circular import
    
    Co-authored-by: rlskoeser <rebecca.s.koeser@princeton.edu>
    Co-authored-by: Julia Damerow <jdamerow@users.noreply.github.com>
    Co-authored-by: Julia Damerow <julia.damerow@gmail.com>
    Co-authored-by: Rebecca Sutton Koeser <rlskoeser@users.noreply.github.com>

--- undate/dateformat/iso8601.py
+++ undate/dateformat/iso8601.py
@@ -1,5 +1,6 @@
 from undate.dateformat.base import BaseDateFormat
 from undate.undate import Undate, UndateInterval
+from typing import Dict, List, Union
 
 
 class ISO8601DateFormat(BaseDateFormat):
@@ -8,41 +9,44 @@ class ISO8601DateFormat(BaseDateFormat):
     # but maybe be permissive, warn if invalid but we can parse
 
     # do not change; Undate relies on this string
-    name = "ISO8601"
+    name: str = "ISO8601"
 
     #: datetime strftime format for known part of date
-    iso_format = {
+    iso_format: Dict[str, str] = {
         "year": "%Y",
         "month": "%m",
         "day": "%d",
     }
 
-    def parse(self, value):
+    def parse(self, value: str) -> Union[Undate, UndateInterval]:
+        # TODO: must return value of type "Union[Undate, UndateInterval]"
         # TODO: what happens if someone gives us a full isoformat date with time?
         # (ignore, error?)
         # TODO: what about invalid format?
         # could be YYYY, YYYY-MM, YYYY-MM-DD, --MM-DD for single date
         # or YYYY/YYYY (etc.) for an interval
-        parts = value.split("/")  # split in case we have a range
+        parts: List[str] = value.split("/")  # split in case we have a range
         if len(parts) == 1:
             return self._parse_single_date(parts[0])
         elif len(parts) == 2:
             return UndateInterval(*[self._parse_single_date(p) for p in parts])
 
-    def _parse_single_date(self, value):
+    def _parse_single_date(self, value: str) -> Undate:
         # split single iso date into parts; convert to int or None
         # special case: missing year
-        date_parts = []
+        date_parts: List[Union[int, None]] = []
         if value.startswith("--"):
             date_parts.append(None)  # year unknown
             value = value[2:]
 
         date_parts.extend([int(p) if p else None for p in value.split("-")])
-        return Undate(*date_parts)
+        # TODO: handle too many parts. Getting this error from type checking:
+        # Argument of type "int | None" cannot be assigned to parameter "formatter" of type "BaseDateFormat | None" in function "__init__"
+        return Undate(*date_parts)  # type: ignore
 
-    def to_string(self, undate):
+    def to_string(self, undate: Undate) -> str:
         # serialize to iso format for simplicity, for now
-        date_parts = []
+        date_parts: List[Union[str, None]] = []
         # for each part of the date that is known, generate the string format
         # then combine
         for date_portion, known in undate.known_values.items():
@@ -53,4 +57,5 @@ class ISO8601DateFormat(BaseDateFormat):
             elif date_portion == "year":
                 # if not known but this is year, add '-' for --MM-DD unknown year format
                 date_parts.append("-")
-        return "-".join(date_parts)
+        # TODO: fix type error: "list[str | None]" is incompatible with "Iterable[str]"
+        return "-".join(date_parts)  # type: ignore
