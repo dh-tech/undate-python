3032785dbf16de32ffcfc6ff51a54eb33bf7c406
rlskoeser <rebecca.s.koeser@princeton.edu>
2024-12-06 10:57:26 +0100

    Fix incorrect import

--- src/undate/converters/calendars/hijri/__init__.py
+++ src/undate/converters/calendars/hijri/__init__.py
@@ -1,4 +1,3 @@
 from undate.converters.calendars.hijri.converter import HijriDateConverter
-from undate.converters.calendars.hebrew.converter import HebrewDateConverter
 
-__all__ = ["HijriDateConverter", "HebrewDateConverter"]
+__all__ = ["HijriDateConverter"]
