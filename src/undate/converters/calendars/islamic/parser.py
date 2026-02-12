bb1d724d01b803b0ad3e04d851e0ecc28240f71c
rlskoeser <rebecca.s.koeser@princeton.edu>
2026-02-12 15:39:04 -0500

    Ignore commas and periods across all grammars

--- src/undate/converters/calendars/islamic/parser.py
+++ src/undate/converters/calendars/islamic/parser.py
@@ -4,6 +4,7 @@ from undate.converters import GRAMMAR_FILE_PATH
 
 grammar_path = GRAMMAR_FILE_PATH / "islamic.lark"
 
-with open(grammar_path) as grammar:
-    # NOTE: LALR parser is faster but can't be used due to ambiguity between years and days
-    islamic_parser = Lark(grammar.read(), start="islamic_date", strict=True)
+# open based on filename to allow relative imports based on grammar file
+islamic_parser = Lark.open(
+    str(grammar_path), rel_to=__file__, start="islamic_date", strict=True
+)
