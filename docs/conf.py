bc91b1d7fb0bfe5ea37ac21913fee90aacbcfe08
rlskoeser <rebecca.s.koeser@princeton.edu>
2026-07-02 15:43:17 -0400

    Remove sphinx_pyodide ext since not yet available

--- docs/conf.py
+++ docs/conf.py
@@ -39,7 +39,6 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.intersphinx",
     "myst_parser",
-    "sphinx_pyodide",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
